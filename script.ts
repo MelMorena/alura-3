a450617c3441b70dfaac7208d0656bcf09e21cd9
MelMorena <128614445+MelMorena@users.noreply.github.com>
2023-06-21 11:31:27 -0300

    script.ts

---
+++ script.ts
@@ -0,0 +1,13 @@
+async function buscaEndereco(cep) {
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente!');
+        }
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        console.log(erro);
+    }
+}
